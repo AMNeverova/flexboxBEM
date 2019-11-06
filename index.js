fd6a1af7b38b05978a0cd135142b33da99c06569
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-06 09:20:04 +0000

    Deploy AMNeverova/flexboxBEM to github.com/AMNeverova/flexboxBEM.git:gh-pages

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 import './src/styles/style.scss';
+
+fetch('https://baconipsum.com/api/?type=all-meat&paras=6').then(response => response.json()).then(response => {
+    let articles = response;
+    const articlesSegment = document.querySelector('.articles');
+    articles.forEach((text, index) => {
+        let textToPaste = text;
+        if (textToPaste.length > 410) {
+            textToPaste = text.slice(0, 410)
+        }
+        let article = createArticle(textToPaste, index);    
+        articlesSegment.append(article);
+    })
+})
+
+function createArticle(text, index) {
+    let newArticle = document.createElement('div');
+    newArticle.classList.add('article');
+    let heading = createHeading(index);
+    let img = createImage(index);
+    let articleContent = createArticleContent(text);
+    newArticle.append(heading);
+    newArticle.append(img);
+    newArticle.append(articleContent);
+    return newArticle;
+}
+
+function createHeading(index) {
+    let heading = document.createElement('h4');
+    let headingClass = index % 4 === 0 || index % 4 === 3 ? 'heading_orange' : 'heading_purple';
+    heading.classList.add('heading', headingClass);
+    let headingText = document.createElement('p');
+    headingText.classList.add('heading__text');
+    headingText.innerHTML = 'about super logo';
+    heading.append(headingText);
+    return heading;
+}
+
+function createImage(index) {
+    let img = document.createElement('img');
+    img.classList.add('article__image');
+    img.setAttribute('alt', 'image');
+    img.setAttribute('src', `https://picsum.photos/140?random=${index}`);
+    return img;
+}
+
+function createArticleContent(text) {
+    let articleContent = document.createElement('div');
+    articleContent.classList.add('article__content');
+    let articleText = document.createElement('p');
+    articleText.classList.add('article__text');
+    articleText.innerHTML = text;
+    let articleLink = document.createElement('a');
+    articleLink.classList.add('article__link-read-more');
+    articleLink.setAttribute('href', '#');
+    articleLink.innerHTML = 'Read more...';
+    articleContent.append(articleText);
+    articleContent.append(articleLink);
+    return articleContent;
+}
